3790db2f44884a8e3de76ee6c322b3f111172154
Chris Zheng <bearzheng2011@gmail.com>
2017-07-09 15:19:42 -0400

    up

--- leetcode/java/two_sum_III.java
+++ leetcode/java/two_sum_III.java
@@ -1,32 +1,32 @@
 public class TwoSum {
 
-    private List<Integer> list = new ArrayList<>();
-    private Map<Integer, Integer> map = new HashMap<>();
+    private Map<Integer, Integer> map;
 
-    // Add the number to an internal data structure.
-	public void add(int number) {
-        Integer count = map.get(number);
-        if (count == null) {
-            count = 0;
-            list.add(number);
-        }
-        map.put(number, count+1);
-	}
-
-    // Find if there exists any pair of numbers which sum is equal to the value.
-	public boolean find(int value) {
-        // iterate list to avoid TLE
-        for (int key : list) {
-            int another = value - key;
-            if ((key != another && map.containsKey(another)) || (key == another && map.get(key) > 1))
-                return true;
+    /** Initialize your data structure here. */
+    public TwoSum() {
+        map = new HashMap<>();
+    }
+    
+    /** Add the number to an internal data structure.. */
+    public void add(int number) {
+        int count = map.getOrDefault(number, 0) + 1;
+        map.put(number, count);
+    }
+    
+    /** Find if there exists any pair of numbers which sum is equal to the value. */
+    public boolean find(int value) {
+        for (int key : map.keySet()) {
+            int left = value - key;
+            int found = map.getOrDefault(left, 0);
+            if ((left == key && found > 1) || (left != key && found > 0)) return true;
         }
         return false;
-	}
+    }
 }
 
-
-// Your TwoSum object will be instantiated and called as such:
-// TwoSum twoSum = new TwoSum();
-// twoSum.add(number);
-// twoSum.find(value);
+/**
+ * Your TwoSum object will be instantiated and called as such:
+ * TwoSum obj = new TwoSum();
+ * obj.add(number);
+ * boolean param_2 = obj.find(value);
+ */
